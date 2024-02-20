1dd01197e3afba92559893e06aad436a1b96209a
ark0723 <ark0723@gmail.com>
2024-02-21 04:38:23 +0900

    flask - pagination 완성, 이미지 깨짐현상 해결

--- kream/app/__init__.py
+++ kream/app/__init__.py
@@ -1,42 +1,104 @@
 from flask import Flask, Blueprint, request, render_template, redirect, url_for
 from product_db.mysql_db import conn_mysql
+from flask_paginate import Pagination, get_page_args
+
 
 app = Flask(__name__, static_url_path="/static")
 
-@app.route("/")
+category_dict = {'1':"outer", "2": "top", "3":"bottom", "4": "shoes", "5": "bag", "6":"wallet", "7":"watch", "8":"accessory"}
+# how many product list would be diplayed on a page?
+per_page = 6
+
+# pagination 참조
+# https://stackoverflow.com/questions/74520043/flask-pagination-without-sqlalchemy
+# https://panda5176.tistory.com/12
+
+# methods = ['GET']
+@app.route('/')
 def index():
+    # 아이템 10개씩 페이지에 보여주기
+    # return(page, per_page, offset)
+    page, _, offset = get_page_args(per_page = per_page)
+    # page: current page(default: 1) -> 페이지 링크 누르면 2, 3, 4, ...
+    # offset: page에 따라 몇번째 데이터부터 보여줄지
+
     conn = conn_mysql()
     cursor = conn.cursor()
-    sql = 'select * from product'
-    cursor.execute(sql)
+
+    # 전체 데이터가 총 몇개인지 체크
+    cursor.execute("select count(*) from product")
+    total_count = cursor.fetchone()['count(*)']
+
+    # 한 페이지에 보여줄 데이터를 가져온다
+    cursor.execute('SELECT * FROM product limit %s offset %s', (per_page, offset))
     kream_item_list = cursor.fetchall()
-    return render_template('index.html', data_list = kream_item_list)
+    
+    # flask의 pagination 객체 생성
+    # https://pythonhosted.org/Flask-paginate/
+    pagination = Pagination(
+        page = page, # 현재 페이지 
+        total = total_count, # 총 몇개의 데이터가 있는지
+        per_page = per_page, # 한 페이지당 몇개 데이터를 보여줄지
+        # prev_label = "<<", # 전 페이지로 가는 링크의 버튼 모양 지정
+        # next_label = ">>", # 후 페이지로 가는 링크의 버튼 모양 지정
+        format_total = True # 총 데이터 중 몇 개의 데이터를 보여주고 있는지 시각화
+    )
+    # search: 페이지 검색 기능, bs_version: bootstrap 사용시 이를 활용할 수 있게 버전을 알려줌
+    return render_template('index.html', data_list = kream_item_list, 
+                           pagination = pagination, search = True, bs_version = 5.3, show_single_page = True)
+    
 
-category_dict = {'1':"outer", "2": "top", "3":"bottom", "4": "shoes", "5": "bag", "6":"wallet", "7":"watch", "8":"accessory"}
-
-
-# 사용자로부터 이메일 받아서 MySql DB에 저장
 @app.route("/filter", methods = ['GET'])
 def filter():
+    # get parameter
+    page, _, offset = get_page_args(per_page = per_page)
     category_id  = request.args.get('category')
     keyword = request.args.get('keyword')
-    print(category_id, keyword)
 
-    # sql db
+    # mysql db
     conn = conn_mysql()
     cursor = conn.cursor()
 
     if category_id != "0": 
-        cursor.execute("select * from product where category = %s and (locate(%s, brand) > 0 or locate(%s, title) > 0)", 
+        # 필터링된 데이터가 총 몇개인지 체크
+        cursor.execute("select count(*) from product where category = %s and (locate(%s, brand) > 0 or locate(%s, title) > 0)", 
                         (category_dict[category_id], keyword, keyword))
+        total_count = cursor.fetchone()['count(*)']
+
+        # 페이지당 보여줄 데이터 불러오기
+        cursor.execute("select * from product where category = %s and (locate(%s, brand) > 0 or locate(%s, title) > 0) limit %s offset %s", 
+                (category_dict[category_id], keyword, keyword, per_page, offset))
         kream_item_list = cursor.fetchall()
-        return render_template('index.html', data_list = kream_item_list)
+
+        pagination = Pagination(
+        page = page, # 현재 페이지 
+        total = total_count, # 총 몇개의 데이터가 있는지
+        per_page = per_page, # 한 페이지당 몇개 데이터를 보여줄지
+        format_total = True # 총 데이터 중 몇 개의 데이터를 보여주고 있는지 시각화
+        )
+        return render_template('index.html', data_list = kream_item_list, 
+                               pagination = pagination, search = True, bs_version = 5.3, show_single_page = True)
 
     else: 
         if keyword:
-            cursor.execute("select * from product where locate(%s, brand) > 0 or (locate(%s, title) > 0)", (keyword, keyword))
+            # 필터링된 데이터가 총 몇개인지 체크
+            cursor.execute("select count(*) from product where locate(%s, brand) > 0 or (locate(%s, title) > 0)", 
+                           (keyword, keyword))
+            total_count = cursor.fetchone()['count(*)']
+
+            # 페이지당 보여줄 데이터 불러오기
+            cursor.execute("select * from product where locate(%s, brand) > 0 or (locate(%s, title) > 0) limit %s offset %s", 
+                           (keyword, keyword, per_page, offset))
             kream_item_list = cursor.fetchall()
-            return render_template('index.html', data_list = kream_item_list)
+
+            pagination = Pagination(
+            page = page, # 현재 페이지 
+            total = total_count, # 총 몇개의 데이터가 있는지
+            per_page = per_page, # 한 페이지당 몇개 데이터를 보여줄지
+            format_total = True # 총 데이터 중 몇 개의 데이터를 보여주고 있는지 시각화
+            )  
+            return render_template('index.html', data_list = kream_item_list, 
+                                pagination = pagination, search = True, bs_version = 5.3, show_single_page = True)
         else: # keyword == ""
             return redirect("/")
 
--- kream/app/static/index.js
+++ kream/app/static/index.js
@@ -3,7 +3,7 @@ function getNowDate(){
   const year = now.getFullYear();
   const month = String(now.getMonth()+1).padStart(2, '0');
   const day = String(now.getDate()).padStart(2, '0');
-  return `Updated on: (${year}-${month}-${day})` 
+  return `Updated: (${year}-${month}-${day})` 
 }
 // Update Dates
 function ShowDate(){
@@ -18,60 +18,19 @@ document.addEventListener("DOMContentLoaded", function(){
   ShowDate()
 })
 
-//checkbox
-const checkBoxes = document.getElementsByClassName("check_btn")
-checkBoxes.forEach(box => {
-  if (box.checked === true){
-    var row = box.parentNode.parentNode;
+
+function hide_row(){
+  // checkbox 자신
+  const check_btn = document.getElementById(ID)
+  // find parent row
+  const row = check_btn.parentElement.parentElement;
+  if (check_btn.checked == true){
     row.style.display = "none";
   }
-});
-
-if (element.checked == true) {
-  condt1[i].parentElement.closest('tr').style = ""
-} else {
-  condt1[i].parentElement.closest('tr').style = "display:none"
 }
 
 
-const rows = document.getElementById("data-table").rows
-rows.forEach(function(row){
-  const checkbox = row.firstChild.firstChild;
-  checkbox.addEventListener("change", function(event){
-    if (event.clicked){
-      row.style.display = "none";
-    }else{
-      row.style.display = "block";
-    }
-  })
-})
 
 
 
-document.querySelectorAll(
- 
-  // Select all rows except the first one 
-  "tr:not(:first-child)").forEach(function (e) {
-
-    // Add onClick event listener
-    // to selected rows
-    e.addEventListener("click", function () {
-
-        // Get all rows except the first one 
-        var rows = 
-            [...document.querySelectorAll(
-              "tr:not(:first-child)"
-            )];
-
-        var notSelectedRows = 
-            rows.filter(function (element) {
-             
-            // Hide the rows that have
-            // not been clicked
-            if (element !== e) {
-                element.style.display = "none";
-            }
-        });
-    });
-});
 
