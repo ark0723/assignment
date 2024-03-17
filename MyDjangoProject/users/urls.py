b6134684082154d73daf3910d47dc9da8379370c
mac <mac@macui-MacBookPro.local>
2024-03-17 19:55:12 +0900

    ML file added

--- MyDjangoProject/users/urls.py
+++ MyDjangoProject/users/urls.py
@@ -1,10 +1,31 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('', views.Users.as_view()),
     path('myinfo/', views.MyInfo.as_view()),
+    # authentication
     path('getToken', obtain_auth_token), #username, password를 보내면 토큰을 반환
+    path("login",views.Login.as_view()), # Django Session Login
+    path("logout", views.Logout.as_view()), # logout
+
+    #JWT Authentication
+    path("login/jwt", views.JWTLogin.as_view()),
+    path("login/jwt/info", views.UserDetailVeiw.as_view()),
+
+    # simple JWT authentication
+    path("login/simpleJWT", TokenObtainPairView.as_view()),
+    path("login/simpleJWT/refresh", TokenRefreshView.as_view()),
+    path("login/simpleJWT/verify", TokenVerifyView.as_view()),
 ]
+
+# {
+#     "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcxMDgxNzk4NSwiaWF0IjoxNzEwMjEzMTg1LCJqdGkiOiJlZWJiNGEwMDdlZTU0MzBkYTIzNTQ4YmQ4NjRlMzhkOCIsInVzZXJfaWQiOjR9.145vMh639sKiC81XQuF-PnqdyhDcbTbzFwn_jWl3rWo",
+#     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEwMjE2Nzg1LCJpYXQiOjE3MTAyMTMxODUsImp0aSI6IjczNmFhMDBkZmE3OTQzNWJhNDkzNDM4ZmIwM2ZkMzNjIiwidXNlcl9pZCI6NH0.7Dficb_UI7YnFxJ800ZQ-wNa8DQvfdYHRuSvfuU_GJQ"
+# }
--- MyDjangoProject/users/views.py
+++ MyDjangoProject/users/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.password_validation import validate_password
 # 로그인한 해당 사용자만 user페이지를 볼 수 있도록 authentication 설정
 from rest_framework.authentication import TokenAuthentication # 토큰 이용한 사용자 인증
 from rest_framework.permissions import IsAuthenticated # 권한부여
+# login / logout
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
 
 
 class Users(APIView):
@@ -61,4 +64,70 @@ class MyInfo(APIView):
             return Response(current_user.errors)
 
 
+class Login(APIView):
+    # api/v1/users/login
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
 
+        if not (username and password):
+            raise ParseError("Both username and password are required")
+
+        user = authenticate(request, username = username, password = password)
+
+        # 인증에 성공했으면
+        if user:
+            login(request, user)
+            return Response(status = status.HTTP_200_OK)
+        else:
+            return Response(status = status.HTTP_403_FORBIDDEN)
+
+class Logout(APIView):
+    # api/v1/users/logout
+    # 현재 로그인한 user인지 체크
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        print("header: ", request.headers)
+        logout(request)
+        return Response(status = status.HTTP_200_OK)
+
+
+#jwt 로그인을 위한 라이브러리
+import jwt
+from django.conf import settings
+
+class JWTLogin(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            raise ParseError("username and password are required")
+
+        user = authenticate(request, username = username, password = password)
+
+        if user:
+            # JWT 토큰 생성을 위한 payload 데이터 준비
+            payload = {"id": user.id, "username": user.username}
+            # JWT 토큰 생성
+            token = jwt.encode(
+                payload,
+                settings.SECRET_KEY,
+                algorithm = "HS256",
+            )
+
+            return Response({"token": token})
+
+from config.authentication import JWTAuthentication
+class UserDetailVeiw(APIView):
+    # JWT 로그인
+    # authentication_classes = [JWTAuthentication]
+
+    # simpleJWT 로그인
+
+    # 로그인 여부 체크
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({"id": user.id, "username":user.username})
